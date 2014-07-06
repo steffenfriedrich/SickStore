63fcc69cb5a65638f6364940b51bd701f7043fed
Wolfram Wingerath <wingerath@informatik.uni-hamburg.de>
2014-07-06 16:15:26 +0200

    .

--- src/database/PIMPClient.java
+++ src/database/PIMPClient.java
@@ -14,12 +14,17 @@ import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.minlog.Log;
 
-import database.handler.ClientReceivedHandler;
 import database.messages.ClientRequestDelete;
 import database.messages.ClientRequestInsert;
 import database.messages.ClientRequestRead;
 import database.messages.ClientRequestScan;
 import database.messages.ClientRequestUpdate;
+import database.messages.ServerResponseDelete;
+import database.messages.ServerResponseException;
+import database.messages.ServerResponseInsert;
+import database.messages.ServerResponseRead;
+import database.messages.ServerResponseScan;
+import database.messages.ServerResponseUpdate;
 import database.messages.exception.DatabaseException;
 
 public class PIMPClient extends Participant {
@@ -257,5 +262,69 @@ public class PIMPClient extends Participant {
     public List<Version> scan(String table, String startkey, int recordcount,
             Set<String> fields) throws DatabaseException {
         return scan(table, startkey, recordcount, fields, true);
+    }
+
+    private class ClientReceivedHandler extends Thread {
+        private Object object;
+        private PIMPClient pimpclient;
+
+        public ClientReceivedHandler(Connection c, Object object,
+                PIMPClient pimpclient) {
+            this.object = object;
+            this.pimpclient = pimpclient;
+        }
+
+        @Override
+        public void run() {
+            if (object instanceof ServerResponseDelete) {
+                ServerResponseDelete message = (ServerResponseDelete) object;
+
+                Long id = message.getId();
+
+                pimpclient.outstandingRequests.put(id, true);
+
+            } else if (object instanceof ServerResponseInsert) {
+                ServerResponseInsert message = (ServerResponseInsert) object;
+
+                Long id = message.getId();
+
+                pimpclient.outstandingRequests.put(id, true);
+
+            } else if (object instanceof ServerResponseRead) {
+                ServerResponseRead message = (ServerResponseRead) object;
+
+                Long id = message.getId();
+                Version version = message.getVersion();
+
+                pimpclient.outstandingRequests.put(id, version);
+
+            } else if (object instanceof ServerResponseScan) {
+                ServerResponseScan message = (ServerResponseScan) object;
+
+                Long id = message.getId();
+                List<Version> entries = message.getEntries();
+
+                pimpclient.outstandingRequests.put(id, entries);
+
+            } else if (object instanceof ServerResponseUpdate) {
+                ServerResponseUpdate message = (ServerResponseUpdate) object;
+
+                Long id = message.getId();
+
+                pimpclient.outstandingRequests.put(id, true);
+
+            } else if (object instanceof ServerResponseException) {
+                ServerResponseException message = (ServerResponseException) object;
+
+                Long id = message.getId();
+                Exception exception = message.getException();
+
+                pimpclient.outstandingRequests.put(id, exception);
+
+            } else {
+                System.out.println("connection " + pimpclient.client.getID()
+                        + " received: " + object);
+            }
+        }
     }
 }
